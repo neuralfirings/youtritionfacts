5147279e50987dd40bfa2032b862124e4bf9802e
Nancy Yi Liang <hi@nyl.io>
2025-04-14 14:56:39 -0700

    bugfix

--- app.py
+++ app.py
@@ -180,22 +180,25 @@ if gcs_client and gcs_bucket:
 
         gb = GridOptionsBuilder.from_dataframe(df)
         gb.configure_column("ytKey", hide=True)
-        # title_link_renderer=JsCode("""
-        #     class UrlCellRenderer {
-        #     init(params) {
-        #         this.eGui = document.createElement('a');
-        #         this.eGui.innerText = params.value;
-        #         this.eGui.setAttribute('href', params.data.link);
-        #         this.eGui.setAttribute('target', '_blank');
-        #         this.eGui.setAttribute('style', 'text-decoration:none; color:#2a5bd7;');
-        #     }
-        #     getGui() {
-        #         return this.eGui;
-        #     }
-        #     }
-        #     """
-        # )
-        title_link_renderer=JsCode('''function(params) {console.log(params);if(params.data.link != undefined) { return `<a href="${params.data.link}" target="_blank">${params.value}</a>`} else { return params.value }}''')
+        title_link_renderer=JsCode("""class UrlCellRenderer {
+init(params) {
+this.eGui = document.createElement('a');
+this.eGui.innerText = params.value;
+if (params.data.link) {                      
+this.eGui.setAttribute('href', params.data.link);
+}
+else{                      
+this.eGui.setAttribute('href', '#');
+}
+this.eGui.setAttribute('target', '_blank');
+this.eGui.setAttribute('style', 'text-decoration:none; color:#2a5bd7;');
+}
+getGui() {
+return this.eGui;
+}
+}"""
+        )
+        # title_link_renderer=JsCode('''function(params) {console.log(params);if(params.data.link != undefined) { return `<a href="${params.data.link}" target="_blank">${params.value}</a>`} else { return params.value }}''')
         gb.configure_column("title", headerName="Title",
             cellRenderer=title_link_renderer,
             maxWidth=300, #suppressSizeToFit=True
